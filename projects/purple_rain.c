e34cb27890f8719beb964a4a9e70a9a39003deb2
Justin Hong <dripcoding@gmail.com>
2024-06-25 22:22:15 -0600

    draw 100 rain drops with random initial positions

--- projects/purple_rain.c
+++ projects/purple_rain.c
@@ -5,6 +5,16 @@ const Color BACKGROUND_COLOR = {230, 230, 250, 255};
 const Color RAIN_COLOR = {138, 43, 226, 255};
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 1000;
+const Vector2 RAIN_DROP_VEL = {0, 5};
+const int RAIN_DROP_WIDTH = 15;
+const int RAIN_DROP_HEIGHT = 50;
+
+typedef struct {
+  Vector2 pos;
+  Vector2 vel;
+  int width;
+  int height;
+} RainDrop;
 
 int main(void) {
   // ===== INITIALIZATION =====
@@ -12,21 +22,37 @@ int main(void) {
 
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Purple Rain");
 
-  Vector2 rainDropPos = {400, 50};
-  Vector2 rainDropVel = {0, 5};
+  RainDrop rainDrops[100];
+
+  for (int i = 0; i < 100; i++) {
+    Vector2 position = {GetRandomValue(10, SCREEN_WIDTH - 10),
+                        GetRandomValue(10, SCREEN_HEIGHT / 10)};
+    rainDrops[i].pos = position;
+    rainDrops[i].vel = RAIN_DROP_VEL;
+    rainDrops[i].width = RAIN_DROP_WIDTH;
+    rainDrops[i].height = RAIN_DROP_HEIGHT;
+  }
 
   // ===== MAIN LOOP =====
   while (!WindowShouldClose()) {
     // ===== UPDATE =====
-    rainDropPos.y += rainDropVel.y;
+    for (int i = 0; i < 100; i++) {
+      rainDrops[i].pos.y += RAIN_DROP_VEL.y;
+    }
 
     // ===== BEGIN DRAWING =====
     BeginDrawing();
 
     ClearBackground(GRAY);
 
-    // define 1 rectangle at the top of the screen
-    DrawRectangle(rainDropPos.x, rainDropPos.y, 50, 100, RAIN_COLOR);
+    for (int i = 0; i < 100; i++) {
+      int x = rainDrops[i].pos.x;
+      int y = rainDrops[i].pos.y;
+      int width = rainDrops[i].width;
+      int height = rainDrops[i].height;
+
+      DrawRectangle(x, y, width, height, RAIN_COLOR);
+    }
 
     EndDrawing();
     // ===== END DRAWING =====
