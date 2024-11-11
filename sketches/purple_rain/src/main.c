cde2a66b2f3dc6e60f5fefbc15b4e2689e04f1d7
Justin Hong <dripcoding@gmail.com>
2024-11-10 17:57:46 -0700

    tweak dimensions of drops

--- sketches/purple_rain/src/main.c
+++ sketches/purple_rain/src/main.c
@@ -5,8 +5,8 @@ const Color BACKGROUND_COLOR = {230, 230, 250};
 const Color RAIN_COLOR = {138, 43, 226, 255};
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 1000;
-const int RAIN_DROP_COUNT = 500;
-const Vector2 RAIN_DROP_VEL = {0, 5};
+const int RAIN_DROP_COUNT = 600;
+const Vector2 RAIN_DROP_VEL = {0, 15};
 const int RAIN_DROP_WIDTH = 7;
 const int RAIN_DROP_HEIGHT = 50;
 
@@ -21,7 +21,7 @@ typedef struct {
 
 int main(void) {
   // ===== INITIALIZATION =====
-  SetTargetFPS(30);
+  SetTargetFPS(60);
 
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Purple Rain");
 
@@ -30,10 +30,10 @@ int main(void) {
   // initialize rain drops
   for (int i = 0; i < RAIN_DROP_COUNT; i++) {
     Vector2 position = {GetRandomValue(10, SCREEN_WIDTH - 10),
-                        GetRandomValue(1, SCREEN_HEIGHT)};
+                        GetRandomValue(0, SCREEN_HEIGHT)};
     RainDrop rainDrop = {
         position, RAIN_DROP_VEL, RAIN_DROP_WIDTH,
-        GetRandomValue(RAIN_DROP_HEIGHT, RAIN_DROP_HEIGHT * 1.75),
+        RAIN_DROP_HEIGHT,
         GetRandomValue(0, 20), 
         RAIN_COLOR
     };
@@ -42,15 +42,17 @@ int main(void) {
 
     // rain drops that are further away from the viewer fall slower and are thinner
     // rain drops that are closer to the viewer fall faster and are thicker
-    if (rainDrops[i].depth > 0 && rainDrops[i].depth <= 5) {
-      rainDrops[i].pos.y += GetRandomValue(RAIN_DROP_VEL.y, 25);
+    if (rainDrops[i].depth > 0 && rainDrops[i].depth <= 10) {
+      rainDrops[i].vel.y = GetRandomValue(3, 5);
       rainDrops[i].width = GetRandomValue(1, 3);
+      rainDrops[i].height = GetRandomValue(10, 30);
+      rainDrops[i].color = MAROON;
     }
     else {
-      rainDrops[i].pos.y += GetRandomValue(25, 50);
-      rainDrops[i].width = GetRandomValue(3, 7);
+      rainDrops[i].vel.y = GetRandomValue(10, 20);
+      rainDrops[i].width = GetRandomValue(5, 7);
+      rainDrops[i].height = GetRandomValue(50, 75);
     }
-
   }
 
   // ===== MAIN LOOP =====
@@ -59,8 +61,11 @@ int main(void) {
     for (int i = 0; i < RAIN_DROP_COUNT; i++) {
       // reset rain drop y position once it reaches the bottom of the screen
       if (rainDrops[i].pos.y >= SCREEN_HEIGHT) {
-        rainDrops[i].pos.y = GetRandomValue(1, SCREEN_HEIGHT / 4);
+        rainDrops[i].pos.x = GetRandomValue(10, SCREEN_WIDTH - 10);
+        rainDrops[i].pos.y = GetRandomValue(0, SCREEN_HEIGHT / 4);
       }
+
+      rainDrops[i].vel.y += 0.002;
 
       rainDrops[i].pos.y += rainDrops[i].vel.y;
     }
