37472b65ddba81688f56c5be66e6065e7f0caa00
Justin Hong <dripcoding@gmail.com>
2024-11-10 19:40:37 -0700

    center star positions around screen center

--- sketches/starfield/src/main.c
+++ sketches/starfield/src/main.c
@@ -2,6 +2,8 @@
 
 const int SCREEN_WIDTH = 1920;
 const int SCREEN_HEIGHT = 1080;
+const int CENTER_X = SCREEN_WIDTH / 2;
+const int CENTER_Y = SCREEN_HEIGHT / 2;
 const int FPS = 60;
 const int STAR_COUNT = 100;
 
@@ -15,16 +17,14 @@ Star* initializeStars() {
   Star* stars = malloc(STAR_COUNT * sizeof(Star));
 
   for (int i = 0; i < STAR_COUNT; i++) {
-	stars[i].x = GetRandomValue(0, SCREEN_WIDTH);
-	stars[i].y = GetRandomValue(0, SCREEN_HEIGHT);
-	stars[i].z = GetRandomValue(0, SCREEN_WIDTH);
+	stars[i].x = GetRandomValue(CENTER_X, SCREEN_WIDTH);
+	stars[i].y = GetRandomValue(CENTER_Y, SCREEN_HEIGHT);
+	stars[i].z = SCREEN_WIDTH;
   }
 
   return stars;
 }
 
-// todo: create static ellipsis to represent starts, centered on screen
-// todo: stars should move outward from the center of the screen with a given speed
 // todo: star size depends on a z value, with closer stars being larger
 
 int main(void) {
@@ -38,6 +38,18 @@ int main(void) {
   // ===== MAIN LOOP =====
   while (!WindowShouldClose()) {
 	// ==== UPDATE START ====
+	// todo: stars should move outward from the center of the screen with a given speed
+
+	float sx;
+	float sy;
+
+	for (int i = 0; i < STAR_COUNT; i++) {
+		if (stars[i].z != 0) {
+			stars[i].z = stars[i].z - 1;
+			// stars[i].x = stars[i].x + 5;
+			// stars[i].y = stars[i].y + 5;
+		}
+	}
 
 	// ==== UPDATE END ====
 
@@ -47,7 +59,9 @@ int main(void) {
 	ClearBackground(BLACK);
 
 	for (int i = 0; i < STAR_COUNT; i++) {
-		DrawCircle(stars[i].x, stars[i].y, 5, WHITE);
+		if (stars[i].z != 0) {
+			DrawCircle(stars[i].x, stars[i].y, 5, WHITE);
+		}
 	}
 
 	// ==== DRAW END ====
