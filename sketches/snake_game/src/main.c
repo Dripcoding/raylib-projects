f4980d6411c5a0724fb66d0c05b610cb49614465
Justin Hong <dripcoding@gmail.com>
2024-11-11 17:56:15 -0700

    add todos

--- sketches/snake_game/src/main.c
+++ sketches/snake_game/src/main.c
@@ -4,6 +4,13 @@
 const int SCREEN_WIDTH = 1920;
 const int SCREEN_HEIGHT = 1080;
 
+// todo: create and initialize the snake with 1 segment
+// todo: create and initialize the food (1 to start with)
+// todo: detect collision between the snake and the food
+// todo: detect collision between the snake and the screen
+// todo: create and initialize the score
+// todo: create and initialize the game over message
+
 int main(void) {
 	// ===== INITIALIZATION START =====
 	SetTargetFPS(60);
