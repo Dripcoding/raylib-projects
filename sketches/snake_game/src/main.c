4f1f0f1abf38a1ae642867ca18f68d533e550f49
Justin Hong <dripcoding@gmail.com>
2025-06-13 14:30:42 -0600

    impl snake segments to follow previous segment pos

--- sketches/snake_game/src/main.c
+++ sketches/snake_game/src/main.c
@@ -7,6 +7,7 @@ const int SCREEN_WIDTH = 1920;
 const int SCREEN_HEIGHT = 1080;
 const int SNAKE_SPEED = 5;
 const int SNAKE_SEGMENT_LENGTH = 5;
+const int MAX_SNAKE_LENGTH = 100;
 
 typedef struct SnakeSegment {
 	Vector2 position;
@@ -19,7 +20,8 @@ typedef struct Snake {
 	Vector2 speed;
 	Vector2 size;
 	Color color;
-	struct SnakeSegment segments[5];
+	struct SnakeSegment segments[10];  // Match MAX_SNAKE_LENGTH
+	int length;
 };
 
 typedef struct Food {
@@ -41,7 +43,7 @@ typedef enum Direction {
 	RIGHT
 };
 
-void updateSnake(struct Snake* snake, enum GameScreen* currentScreen, enum Direction* currentDirection) {
+void updateSnake(struct Snake* snake, enum GameScreen* currentScreen, enum  Direction* currentDirection) {
 	int key = GetKeyPressed();
 
 	// snake movement with arrow keys
@@ -49,6 +51,16 @@ void updateSnake(struct Snake* snake, enum GameScreen* currentScreen, enum Direc
 	else if (IsKeyPressed(KEY_LEFT) && *currentDirection != RIGHT) *currentDirection = LEFT;
 	else if (IsKeyPressed(KEY_UP) && *currentDirection != DOWN) *currentDirection = UP;
 	else if (IsKeyPressed(KEY_DOWN) && *currentDirection != UP) *currentDirection = DOWN;
+
+	// Update segments to follow the previous segment
+	for (int i = snake->length - 1; i > 0 && i < 10; i--) {
+		snake->segments[i].position = snake->segments[i - 1].position;
+	}
+	
+	// Update first segment to follow the head
+	if (snake->length > 0) {
+		snake->segments[0].position = snake->position;
+	}
 
 	switch (*currentDirection) {
 		case UP:
@@ -72,16 +84,7 @@ void updateSnake(struct Snake* snake, enum GameScreen* currentScreen, enum Direc
 		snake->position.y >= SCREEN_HEIGHT || snake->position.y <= 0) {
 		*currentScreen = GAMEOVER;
 	}
-
-	// updateSnakeSegments(snake);
 }
-
-// void updateSnakeSegments(struct Snake* snake) {
-// 	for (int i = 0; i < SNAKE_SEGMENT_LENGTH; i++) {
-// 		snake -> segments[i].position.x = snake -> position.x + ((i + 1) * (snake -> size.x));
-// 		snake -> segments[i].position.y = snake -> position.y + ((i + 1) * (snake -> size.y));
-// 	}
-// }
 
 int main(void) {
 	// ===== INITIALIZATION START =====
@@ -95,10 +98,11 @@ int main(void) {
 		.size = {20, 20},
 		.speed = {SNAKE_SPEED, SNAKE_SPEED},
 		.color = MAROON,
+		.length = 1
 	};
 
 	struct Food food = {
-		.position = {GetRandomValue(0, SCREEN_WIDTH), GetRandomValue(0, SCREEN_HEIGHT)},
+		.position = {GetRandomValue(20, SCREEN_WIDTH - 20), GetRandomValue(20, SCREEN_HEIGHT - 20)},
 		.size = {20, 20},
 		.color = GREEN,
 	};
@@ -122,11 +126,12 @@ int main(void) {
 				}
 				break;
 			case GAMEPLAY:
-				break;
+				break;			
 			case GAMEOVER:
 				score = 0;
 				if (IsKeyPressed(KEY_ENTER)) {
 					currentScreen = GAMEPLAY;
+					snake.length = 1;
 				}
 				break;
 			default:
@@ -148,14 +153,15 @@ int main(void) {
 				break;
 			case GAMEPLAY:
 				DrawRectangle(0,0,SCREEN_WIDTH, SCREEN_HEIGHT, BLACK);
-				break;
+				break;			
 			case GAMEOVER:
 				DrawRectangle(0,0,SCREEN_WIDTH, SCREEN_HEIGHT, BLUE);
 				DrawText("Game Over", 190, 200, 50, BLACK);
 				DrawText("Press [ENTER] to play again", 190, 250, 20, BLACK);
 
-				// reset snake position
+				// reset snake position and length
 				snake.position = (Vector2){SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
+				snake.length = 1;
 
 				break;
 			default:
@@ -182,20 +188,12 @@ int main(void) {
 				CloseWindow();
 			}
 
-			free(buffer);
-
+			free(buffer);			
 			DrawRectangleV(snake.position, snake.size, snake.color);	
 			// DRAW SCORE END
 
 			// DRAW SNAKE SEGMENTS START
-			struct Snakesegment* segments = calloc(score, sizeof(struct SnakeSegment));
-			if(segments == NULL) {
-				printf("Error: realloc failed\n");
-				CloseWindow();
-			}
-			for (int i = 0; i < score; i++) {
-				snake.segments[i].position.x = snake.position.x - ((i + 1) * (snake.size.x)) - 5;
-				snake.segments[i].position.y = snake.position.y - 5;
+			for (int i = 0; i < snake.length; i++) {
 				snake.segments[i].size = snake.size;
 				snake.segments[i].color = WHITE;
 				DrawRectangleV(snake.segments[i].position, snake.segments[i].size, snake.segments[i].color);
@@ -205,10 +203,12 @@ int main(void) {
 			// DETECT COLLISION BETWEEN SNAKE AND FOOD START
 			Rectangle snakeRect = {snake.position.x, snake.position.y, snake.size.x, snake.size.y};
 			Rectangle foodRect = {food.position.x, food.position.y, food.size.x, food.size.y};
-			
 			if (CheckCollisionRecs(snakeRect, foodRect) == true) {
-				printf("Collision detected\n");
 				score += 1;
+				// Grow the snake by increasing its length
+				if (snake.length < MAX_SNAKE_LENGTH - 1) {
+					snake.length++;
+				}
 				food.position = (Vector2){GetRandomValue(0, SCREEN_WIDTH), GetRandomValue(0, SCREEN_HEIGHT)};
 			} else {
 				if (currentScreen == 1) {
